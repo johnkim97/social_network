50edffbe7b9ab5975c772b853f643590d13896cf
john kim <johnkim@johns-MacBook-Air.local>
2013-08-05 06:14:11 -0500

    first commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :name, :group_id, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialNetwork::Application.config.secret_key_base = '6bb0125501fd7b904a6c052c6d39e3618b7968344bff08616557e113e18c29e787749f3fdd07349193b443c4f88b16057e0a5cd9f30d8454952538ac0c8ec5c4'
